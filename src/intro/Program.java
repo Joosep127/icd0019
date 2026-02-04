be908d56b518f428de21bd96697bd792838cf3dc
Joosep127 <joosep.tomingas@gmail.com>
2026-02-04 16:08:12 +0200

    geier

--- src/intro/Program.java
+++ src/intro/Program.java
@@ -14,12 +14,22 @@ public class Program {
     }
 
     public static int asInteger(String input) {
-        return 0;
+        int num = 0;
+        for (int i = input.length()-1; i >= 0; i--)  {
+          if (input.charAt(i) == '1') {
+            System.out.println(i);
+            num += pow(2, i);
+          }
+        }
+        return num;
     }
 
     private static int pow(int arg, int power) {
         // Java has Math.pow() but this time write your own implementation.
-
-        return 0;
+        int num = 1;
+        for (int i = 1; i < power; i++) {
+          num *= arg;
+        }
+        return num;
     }
 }
