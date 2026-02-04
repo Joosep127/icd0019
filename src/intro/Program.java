9411f88331dccb8122e55c26661772e9fc093f06
Joosep127 <joosep.tomingas@gmail.com>
2026-02-04 16:28:19 +0200

    done

--- src/intro/Program.java
+++ src/intro/Program.java
@@ -7,17 +7,24 @@ public class Program {
         int integer = asInteger("11001101");
 
         System.out.println(integer); // 205
+        System.out.println(asBinaryString(integer));
     }
 
     public static String asBinaryString(int input) {
-        return "";
+        int temp;
+        StringBuilder sb = new StringBuilder("");
+        while (0 < input) {
+          temp = input % 2;
+          input = input / 2;
+          sb.insert(0, temp);
+        }
+        return sb.toString();
     }
 
     public static int asInteger(String input) {
         int num = 0;
         for (int i = input.length()-1; i >= 0; i--)  {
-          if (input.charAt(i) == '1') {
-            System.out.println(i);
+          if (input.charAt(input.length()-1-i) == '1') {
             num += pow(2, i);
           }
         }
@@ -27,7 +34,7 @@ public class Program {
     private static int pow(int arg, int power) {
         // Java has Math.pow() but this time write your own implementation.
         int num = 1;
-        for (int i = 1; i < power; i++) {
+        for (int i = 1; i < power+1; i++) {
           num *= arg;
         }
         return num;
