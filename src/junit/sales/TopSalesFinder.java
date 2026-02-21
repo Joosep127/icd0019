ecb7df173b94bad64c0dfb17798e8895ec1f4f16
Joosep127 <48839946+Joosep127@users.noreply.github.com>
2026-02-21 18:58:19 +0200

    asdf

--- src/junit/sales/TopSalesFinder.java
+++ src/junit/sales/TopSalesFinder.java
@@ -2,43 +2,106 @@ package junit.sales;
 
 public class TopSalesFinder {
 
+    SalesRecord[] listSR = new SalesRecord[0];
+
     public void registerSale(SalesRecord record) {
 
-        // store sales record for later analyses by findItemsSoldOver()
+        int size = listSR.length;
+        SalesRecord[] newSR = new SalesRecord[size + 1];
 
+        for (int i = 0; i < size; i++) {
+            newSR[i] = listSR[i];
+        }
+
+        newSR[size] = record;
+        listSR = newSR;
+    }
+
+    public SalesRecordResult[] ListSRToListRecordResult(SalesRecord[] listSR) {
+        SalesRecordResult[] listSRR = new SalesRecordResult[listSR.length];
+        SalesRecord sr;
+        for (int i = 0; i < listSR.length; i++) {
+            sr = listSR[i];
+            listSRR[i] = new SalesRecordResult(sr.productId(), sr.itemsSold());
+        }
+        //return listSRR;
+        return new SalesRecordResult[] {new SalesRecordResult("gei",1)};
     }
 
     public SalesRecordResult[] findItemsSoldOver(int amount) {
 
-        // find ids of records that sold over specified amount.
+        SalesRecord[] filtered = new SalesRecord[0];
 
-        return new SalesRecordResult[0];
+        for (SalesRecord sr : listSR) {
+            if (sr.itemsSold() > amount) {
+
+                SalesRecord[] newArr = new SalesRecord[filtered.length + 1];
+
+                for (int i = 0; i < filtered.length; i++) {
+                    newArr[i] = filtered[i];
+                }
+
+                newArr[filtered.length] = sr;
+                filtered = newArr;
+            }
+        }
+
+        return ListSRToListRecordResult(filtered);
     }
 
-    public void removeSalesRecordsFor(String id) {
+    public void removeSalesRecordsFor(String productId) {
+        int count = 0;
+        for (SalesRecord sr : listSR) {
+            if (!sr.productId().equals(productId)) {
+                count++;
+            }
+        }
 
-        // removes records with specified id
+        SalesRecord[] newSR = new SalesRecord[count];
+
+        int i = 0;
+        for (SalesRecord sr : listSR) {
+            if (!sr.productId().equals(productId)) {
+                newSR[i] = sr;
+                i++;
+            }
+        }
+
+        listSR = newSR;
     }
-
     public SalesRecord[] getAllRecordsPaged(int pageNumber, int pageSize) {
+        SalesRecord[] paginatedSR;
+        int startingIdx = (pageNumber - 1) * pageSize;
 
-        // return a slice of all stored records
+        if (startingIdx < 0 || listSR.length < startingIdx) {
+            return new SalesRecord[0];
+        }
 
-        return new SalesRecord[0];
+        int remainingRecords = listSR.length - startingIdx;
+        int currentPageSize = Math.min(pageSize, remainingRecords);
+        paginatedSR = new SalesRecord[currentPageSize];
+
+        for (int i = 0; i < currentPageSize; i++) {
+            paginatedSR[i] = listSR[i+startingIdx];
+        }
+
+        return paginatedSR;
     }
 
     public int getRecordCount() {
+
         // only needed for the sample application
 
         // returns the count of all records
 
-        return 0;
+        return listSR.length;
     }
 
     public void removeRecord(String id) {
         // only needed for the sample application
 
         // removes record with specific id
+        removeSalesRecordsFor(id);
     }
 
 }
