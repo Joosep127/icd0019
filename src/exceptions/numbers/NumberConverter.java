05de3cbd8c81ce83ddd33714811e51875e0eccb1
Joosep127 <joosep.tomingas@gmail.com>
2026-03-09 00:36:12 +0200

    geigei

--- src/exceptions/numbers/NumberConverter.java
+++ src/exceptions/numbers/NumberConverter.java
@@ -1,7 +1,5 @@
 package exceptions.numbers;
 
-import org.junit.platform.console.shadow.picocli.CommandLine;
-
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -11,14 +9,15 @@ import java.util.Properties;
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.util.List;
+import java.util.Map;
 
 public class NumberConverter {
   List<String> expected;
   Properties properties;
   String lang;
+  boolean skipFuture;
+  boolean addingAfterDelim;
 
   public NumberConverter(String lang) {
     this.lang = lang;
@@ -31,7 +30,9 @@ public class NumberConverter {
     }
     try {
       expected = getExpected();
-    } catch (IOException ignored) {
+    } catch (IOException e) {
+      expected = List.of();
+      System.err.println("Warning: expected file missing for " + lang);
     }
   }
 
@@ -69,17 +70,86 @@ public class NumberConverter {
     }
   }
 
-  public String getNthLine(int n) {
+  private String getNthLine(int n) {
     return expected.get(n);
   }
 
-  public String numberInWords(Integer number) {
+  private String getProp(String s) {
+    return properties.getProperty(s);
+  }
+
+  private String getProp(String s, String def) {
+    return properties.getProperty(s, def);
+  }
+
+  private String checkExpectation(int number, String def) {
+    return getProp(String.valueOf(number), def);
+  }
+
+  private String str(int num) {
+    return String.valueOf(num);
+  }
+
+  private String applyPrefixSuffixAndDelimiters(String[] sufpre, String[] deLims, int digit, int number) {
+    if (skipFuture) { // We know for a fact that we're on the teens here
+      return deLims[0] + digit + sufpre[1];
+    }
+
+    return sufpre[0] + digit + deLims[0] + sufpre[1] + deLims[1];
+  }
+
+  private String applyNumberRules(int key, String value, int number) {
+    number = number - (number / (key * 10)); // Gets rid of already checked numbers of numbers that are beyond the range
+                                             // of which I'm expected to turn in to strings
+
+    if (skipFuture || addingAfterDelim)
+      return "";
+    if (number == 0) {
+      skipFuture = true;
+    }
+
+    int digit = number / key;
+    addingAfterDelim = number == digit * key;
+
+    if (key == 10 && digit == 1 && addingAfterDelim) {
+      if (getProp("teen") != null) {
+        skipFuture = true;
+        value = "teen";
+      }
+    }
+
+    String beforeDelim = getProp(value + "-before-delimiter", "");
+    String afterDelim = addingAfterDelim ? getProp(value + "-after-delimiter", "") : "";
+    String fullWord = getProp(value);
+    String prefix = fullWord != null ? "" : getProp(value + "-prefix", "");
+    String suffix = fullWord != null ? fullWord : getProp(value + "-suffix", "");
+
+    String[] deLims = { beforeDelim, afterDelim };
+    String[] sufpre = { prefix, suffix };
+
+    return checkExpectation(number, applyPrefixSuffixAndDelimiters(sufpre, deLims, digit, number));
+  }
+
+  public String numberInWords(int number) {
     if (properties == null) {
       throw new MissingTranslationException(lang);
     }
     if (-1 < number && number < 131) {
       return getNthLine(number);
     }
-    return null;
+    addingAfterDelim = false;
+    skipFuture = false;
+
+    StringBuilder sb = new StringBuilder();
+    Map<Integer, String> values = Map.of(
+        1000000000, "billion",
+        1000000, "million",
+        1000, "thousand",
+        100, "hundred",
+        10, "tens",
+        1, "ones");
+    values.forEach((key, value) -> sb.append(applyNumberRules(key, value, number)));
+
+    return sb.toString();
   }
 }
