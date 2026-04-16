ac783a5b413a2d835ae16473d58d4d2261b6f5a6
Joosep127 <joosep.tomingas@gmail.com>
2026-04-16 03:42:19 +0300

    gei

--- src/bonus/installments/InstallmentGenerator.java
+++ src/bonus/installments/InstallmentGenerator.java
@@ -1,6 +1,7 @@
 package bonus.installments;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 public class InstallmentGenerator {
@@ -8,6 +9,51 @@ public class InstallmentGenerator {
     public List<Installment> generateRowsFor(
             Integer amount, LocalDate periodStart, LocalDate periodEnd) {
 
-        throw new RuntimeException("not implemented yet");
+        if (amount < 3) {
+            return List.of(new Installment(amount, periodStart));
+        }
+
+        int months = periodEnd.getDayOfMonth() - periodStart.getDayOfMonth() + (periodEnd.getYear() - periodStart.getYear()) * 12;
+
+        int tempPay = amount/months;
+        int normalPay;
+        int abovePay;
+
+        int aboveMonths;
+        int belowMonths;
+
+        if (amount/months < 3) {
+            normalPay = 3;
+            abovePay = 4;
+
+            aboveMonths = amount%3;
+            belowMonths = (amount/3)-1;
+        } else {
+            normalPay = tempPay;
+            abovePay = normalPay + 1;
+
+            aboveMonths = amount%months;
+            belowMonths = (months-aboveMonths)-1;
+        }
+
+        List<Installment> output = new ArrayList<Installment>();
+        output.add(new Installment(normalPay, periodStart));
+
+
+        LocalDate start = periodStart.withDayOfMonth(1);
+
+        for (int i = 0; i < belowMonths; i++) {
+            start = start.plusMonths(1);
+
+            output.add(new Installment(normalPay, start));
+        }
+
+        for (int i = 0; i < aboveMonths; i++) {
+            start = start.plusMonths(1);
+
+            output.add(new Installment(abovePay, start));
+        }
+
+        return output;
     }
 }
